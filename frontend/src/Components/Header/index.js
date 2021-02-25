c518178e70c3f95e0c604c35d067dd3772e5d779
Ross Ketron <rossketron@gmail.com>
2021-02-25 12:40:05 -0500

    Updated and fixed the header component and dropdown links

--- frontend/src/Components/Header/index.js
+++ frontend/src/Components/Header/index.js
@@ -1,58 +1,29 @@
 import React from "react"
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
-import { Link, Menu, MenuItem } from "@material-ui/core"
-
-const useStyles = makeStyles((theme) =>
-    createStyles({
-        container: {
-            position: "fixed",
-            top: 0,
-            right: 0,
-            margin: "0 30px",
-            background: "none",
-            MaxWidth: "400px",
-            height: "120px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-evenly",
-            alignItems: "center",
-        },
-        titleWrapper: {
-            fontSize: "24px",
-            color: "#f0f0f0",
-            dropShadow: "30px",
-        },
-        navLinkWrapper: {
-            fontSize: "24px",
-            dropShadow: "30px",
-        },
-        navLink: {
-            margin: "0 20px",
-            color: "#f0f0f0",
-            textDecoration: "bolder",
-            "&:hover": {
-                cursor: "pointer",
-                textDecorationLine: "none",
-                color: "red",
-            }
-        },
-        
-    })
-);
+import "./index.css"
 
 const Header = () => {
-    const style = useStyles()
-
     return (
         <React.Fragment>
-            <div className={style.container}>
-                <div className={style.titleWrapper}>
+            <div className="container">
+                <div className="titleWrapper">
                     <h1>Easy Recommendation</h1>
                 </div> 
-                <div className={style.navLinkWrapper}>
-                    <Link href="/" className={style.navLink}>Home</Link>
-                    <Link href="#" className={style.navLink}>Account</Link>
-                    <Link href="#" className={style.navLink}>Resources</Link>
+                <div className="navLinkWrapper">
+                    <a href="/" className="navLink">Home</a>
+                    <div className="dropdown">
+                        <button className="navLinkWrapper">Account</button>
+                        <div className="dropdown-content">
+                            <a href="#">Log In</a>
+                            <a href="#">Sign Up</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button className="navLinkWrapper">Resources</button>
+                        <div className="dropdown-content">
+                            <a href="#">Templates</a>
+                            <a href="#">FAQ</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
