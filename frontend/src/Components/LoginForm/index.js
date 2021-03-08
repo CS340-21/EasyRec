a6d7721203b1e152289fc0119ee70d99836ca66b
Jovan Hernandez <jherna18@vols.utk.edu>
2021-03-07 19:29:17 -0500

    created form for login page #6

--- frontend/src/Components/LoginForm/index.js
+++ frontend/src/Components/LoginForm/index.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import { TextField, Button } from "@material-ui/core";
 import "./index.css";
 
 export class LoginForm extends Component {
   render() {
     return (
-      <div className="formContainer">
-        <h2>Log In </h2>
-      </div>
+      <>
+        <div className="formContainer">
+          <form className="loginForm" action="login" method="post">
+            <div className="entryField">
+              {/* <h2>Username</h2> */}
+              <TextField id="username" label="Username" variant="standard" />
+            </div>
+            <div className="entryField">
+              {/* <h2>Password</h2> */}
+              <TextField id="password" label="Password" variant="standard" />
+            </div>
+            <div className="submitButton">
+              <Button variant="contained">Login</Button>
+            </div>
+          </form>
+        </div>
+      </>
     );
   }
 }
