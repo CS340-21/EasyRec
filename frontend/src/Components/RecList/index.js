12aa25ae0a70d6fcd13a0c6aeb9915062a87ab52
Ross Ketron <rossketron@gmail.com>
2021-03-15 08:52:30 -0400

    started updating letters list, need more work

--- frontend/src/Components/RecList/index.js
+++ frontend/src/Components/RecList/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { createStyles, makeStyles, Theme, withStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles, withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
