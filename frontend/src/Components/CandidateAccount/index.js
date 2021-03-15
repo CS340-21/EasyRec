2c87b7c39dfb59f144c5639fb8c8d1090037b8b6
Ross Ketron <rossketron@gmail.com>
2021-03-15 08:51:22 -0400

    Added basic user page with responsiveness

--- frontend/src/Components/CandidateAccount/index.js
+++ frontend/src/Components/CandidateAccount/index.js
@@ -6,16 +6,18 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import List from '@material-ui/core/List';
+import HomeIcon from '@material-ui/icons/Home';
+import List from "@material-ui/core/List";
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
+import SendIcon from '@material-ui/icons/Send';
+import ListIcon from '@material-ui/icons/ViewList';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import RecList from '../RecList';
 
 const drawerWidth = 240;
 
@@ -58,6 +60,10 @@ function ResponsiveDrawer(props) {
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
+  // Set main content to user home page default.
+  // options are 'Home', 'Letters', 'Request'
+  const [mainContent, setMainContent] = React.useState("Home");
+
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
@@ -67,21 +73,18 @@ function ResponsiveDrawer(props) {
       <div className={classes.toolbar} />
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
+          <ListItem button key="Home" onClick={() => setMainContent("Home")}>
+            <ListItemIcon><HomeIcon /></ListItemIcon>
+            <ListItemText primary="Home" />
           </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
+          <ListItem button key="Letters" onClick={() => setMainContent("Letters")}>
+            <ListItemIcon><ListIcon /></ListItemIcon>
+            <ListItemText primary="Letters" />
           </ListItem>
-        ))}
+          <ListItem button key="Request" onClick={() => setMainContent("Request")}>
+            <ListItemIcon><SendIcon /></ListItemIcon>
+            <ListItemText primary="Request" />
+          </ListItem>
       </List>
     </div>
   );
@@ -140,29 +143,44 @@ function ResponsiveDrawer(props) {
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        {(() => {
+          switch (mainContent) {
+
+            case "Letters":
+              return ( <RecList /> );
+
+            case "Request":
+              return (
+                <Typography paragraph>
+                  This is request content!!  Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                  facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                  tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                  consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+                  vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+                  hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+                  tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+                  nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+                  accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+                </Typography>
+              );
+
+            default: //this includes initial state: 'Home'
+              return (
+                <Typography paragraph>
+                  This is account home content!!  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                  ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                  facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                  gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                  donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                  adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                  Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                  imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                  arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                  donec massa sapien faucibus et molestie ac.
+                </Typography>
+              );
+          }
+        })()}
       </main>
     </div>
   );
