bebfc7b3e1fb22ae98783a33681fc7fe0245b470
Henry Eigen <eigen.henry@gmail.com>
2021-03-19 16:59:15 -0500

    Update test_populate.sh

--- website/test_populate.sh
+++ website/test_populate.sh
@@ -1,5 +1,5 @@
-http POST http://127.0.0.1:8000/register/ email=henry@yahoo.com password=birdcat615 first_name=henry last_name=eigen
+http POST http://127.0.0.1:8000/api/register/ email=henry@yahoo.com password=birdcat615 first_name=henry last_name=eigen
 
-http POST http://127.0.0.1:8000/register/ email=james@gmail.com password=hammer first_name=james last_name=smith
+http POST http://127.0.0.1:8000/api/register/ email=james@gmail.com password=hammer first_name=james last_name=smith
 
-http POST http://127.0.0.1:8000/register/ email=sally@aol.com password=1234abcd first_name=sally last_name=draper
+http POST http://127.0.0.1:8000/api/register/ email=sally@aol.com password=1234abcd first_name=sally last_name=draper
